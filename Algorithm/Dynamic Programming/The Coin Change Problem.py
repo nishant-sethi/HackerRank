ac6d2c85cdeb31a8e248f48dc8bb6684cadf5ce1
Nishant sethi <nsethi610@gmail.com>
2018-08-19 23:56:00 +0530

    Add files via upload

---
+++ Algorithm/Dynamic Programming/The Coin Change Problem.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Dynamic Programming Python implementation of Coin 
+# Change problem
+def count(S, m, n):
+ 
+    # table[i] will be storing the number of solutions for
+    # value i. We need n+1 rows as the table is constructed
+    # in bottom up manner using the base case (n = 0)
+    # Initialize all table values as 0
+    table = [0 for k in range(n+1)]
+ 
+    # Base case (If given value is 0)
+    table[0] = 1
+ 
+    # Pick all coins one by one and update the table[] values
+    # after the index greater than or equal to the value of the
+    # picked coin
+    for i in range(0,m):
+        for j in range(S[i],n+1):
+            table[j] += table[j-S[i]]
+ 
+    return table[n]
+    return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
