b41787672de8a318870b72bd9ffaa7c5fd5e5601
Nishant sethi <nsethi610@gmail.com>
2018-08-29 18:28:48 +0530

    Update Day 23 BST Level-Order Traversal.py

--- 30 days of code/Day 23 BST Level-Order Traversal.py
+++ 30 days of code/Day 23 BST Level-Order Traversal.py
@@ -1,27 +1,13 @@
-    def levelOrder(self,root):
-        #Write your code here
-        h = self.height(root)
-        for i in range(1, h+1):
-            self.printGivenLevel(root, i)
-    
-    def height(self,node):
-        if node is None:
-            return 0
-        else :
-            # Compute the height of each subtree 
-            lheight = self.height(node.left)
-            rheight = self.height(node.right)
-
-            #Use the larger one
-            if lheight > rheight :
-                return lheight+1
-            else:
-                return rheight+1
-    def printGivenLevel(self,root ,level):
-        if root is None:
-            return
-        if level == 1:
-            print(root.data,end=" ")
-        elif level > 1 :
-            self.printGivenLevel(root.left , level-1)
-            self.printGivenLevel(root.right , level-1)
+def levelOrder(self,root):
+    #Write your code here
+    queue=[]
+    not_visited=[root]
+    while len(not_visited)!=0:
+        x=not_visited.pop(0)
+        queue.append(x)
+        if x.left is not None:
+            not_visited.append(x.left)
+        if x.right is not None:
+            not_visited.append(x.right)
+    for i in queue:
+        print(i.data,end=" ")
