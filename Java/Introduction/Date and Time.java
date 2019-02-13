a0d4ea0ca67ae35a28ed0e7647d5ad0751ab326b
Nishant sethi <nsethi610@gmail.com>
2019-02-13 10:36:29 +0530

    Update Date and Time.java

--- Java/Introduction/Date and Time.java
+++ Java/Introduction/Date and Time.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class Solution{
     public static String getDay(String day, String month, String year){
-        LocalDate date = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        LocalDate date = LocalDate.of((year),(month),(day));
         return String.valueOf(date.getDayOfWeek());
 
     }
