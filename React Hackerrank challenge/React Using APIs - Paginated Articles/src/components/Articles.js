a4c3b996c73cd1359bbb8fc7849a703970d9fd7b
nishantsethi <nsethi610@gmail.com>
2023-12-28 21:56:12 +0000

    fixed react paginated articles

--- React Hackerrank challenge/React Using APIs - Paginated Articles/src/components/Articles.js
+++ React Hackerrank challenge/React Using APIs - Paginated Articles/src/components/Articles.js
@@ -1,68 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-class Articles extends React.Component {
-  state = {
-    totalPages: 1,
-    currentPage: 1,
-    articles: [],
-  };
-  componentDidMount() {
-    fetch(
-      `https://jsonmock.hackerrank.com/api/articles?page=${this.state.currentPage}`
-    )
-      .then((res) => res.json())
-      .then((data) =>
-        this.setState({
-          totalPages: data.total_pages,
-          articles: data.data,
-          currentPage: data.page,
-        })
-      );
-  }
-  getArticles(page) {
-    if (page !== this.state.currentPage) {
+function Articles() {
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPages] = useState(1);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fn = () => {
+      fetch(`https://jsonmock.hackerrank.com/api/articles?page=${currentPage}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTotalPages(data.total_pages);
+          setArticles(data.data);
+          setCurrentPages(data.page);
+        });
+    };
+    fn();
+  }, []);
+  function getArticles(page) {
+    if (page !== currentPage) {
       fetch(`https://jsonmock.hackerrank.com/api/articles?page=${page}`)
         .then((res) => res.json())
         .then((data) => {
-          this.setState({
-            totalPages: data.total_pages,
-            articles: data.data,
-            currentPage: page,
-          });
+          setTotalPages(data.total_pages);
+          setArticles(data.data);
+          setCurrentPages(data.page);
         });
     }
   }
-  render() {
-    const { totalPages, articles } = this.state;
-    return (
-      <React.Fragment>
-        <div className="pagination">
-          {Array(totalPages)
-            .fill()
-            .map((_, i) => (
-              <button
-                data-testid="page-button"
-                key={`page-button-${i + 1}`}
-                onClick={() => this.getArticles(i + 1)}
-              >
-                {i + 1}
-              </button>
-            ))}
-        </div>
-        <ul className="results">
-          {articles
-            .filter(
-              (article) => article.title !== null && article.title.length > 0
-            )
-            .map((article, i) => (
-              <li key={`title-${i}`} data-testid="result-row">
-                {article.title}
-              </li>
-            ))}
-        </ul>
-      </React.Fragment>
-    );
-  }
+  return (
+    <>
+      <div className='pagination'>
+        {Array(totalPages)
+          .fill()
+          .map((_, i) => (
+            <button
+              data-testid='page-button'
+              key={`page-button-${i + 1}`}
+              onClick={() => getArticles(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+      </div>
+      <ul className='results'>
+        {articles
+          .filter(
+            (article) => article.title !== null && article.title.length > 0
+          )
+          .map((article, i) => (
+            <li key={`title-${i}`} data-testid='result-row'>
+              {article.title}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
 
 export default Articles;
