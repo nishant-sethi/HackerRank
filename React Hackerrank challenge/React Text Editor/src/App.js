6f09ba29a15c99a5d19406fdf36f8d53f00d5953
nishantsethi <nsethi610@gmail.com>
2023-12-28 21:44:51 +0000

    fixed react text editor

--- React Hackerrank challenge/React Text Editor/src/App.js
+++ React Hackerrank challenge/React Text Editor/src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
 import './App.css';
 
 import TextEditor from './components/TextEditor';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TextEditor />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <TextEditor />
+    </div>
+  );
 }
 
 export default App;
