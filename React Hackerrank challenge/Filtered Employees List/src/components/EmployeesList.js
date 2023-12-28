292e6c215aef208f35d8dea488c33db5d1cdb62e
nishantsethi <nsethi610@gmail.com>
2023-12-28 21:06:24 +0000

    fixed react filter employee list to use latest dependencies version

--- React Hackerrank challenge/Filtered Employees List/src/components/EmployeesList.js
+++ React Hackerrank challenge/Filtered Employees List/src/components/EmployeesList.js
@@ -1,38 +1,32 @@
-import React from "react";
+import { useState } from 'react';
 
-class EmployeesList extends React.Component {
-  state = {
-    empList: this.props.employees,
-  };
-  getFilteredList(empList, val) {
+function EmployeesList({ employees }) {
+  const [empList, setEmpList] = useState(employees);
+
+  function getFilteredList(empList, val) {
     const filterEmps = empList.filter((emp) =>
       emp.name.toUpperCase().includes(val.toUpperCase())
     );
-    this.setState({ empList: filterEmps });
+    setEmpList(filterEmps);
   }
-
-  render() {
-    const { employees } = this.props;
-    return (
-      <React.Fragment>
-        <div className="controls">
-          <input
-            type="text"
-            className="filter-input"
-            data-testid="filter-input"
-            onChange={(e) => this.getFilteredList(employees, e.target.value)}
-          />
-        </div>
-        <ul className="employees-list">
-          {this.state.empList.map((employee) => (
-            <li key={employee.name} data-testid="employee">
-              {employee.name}
-            </li>
-          ))}
-        </ul>
-      </React.Fragment>
-    );
-  }
+  return (
+    <>
+      <div className='controls'>
+        <input
+          type='text'
+          className='filter-input'
+          data-testid='filter-input'
+          onChange={(e) => getFilteredList(employees, e.target.value)}
+        />
+      </div>
+      <ul className='employees-list'>
+        {empList.map((employee) => (
+          <li key={employee.name} data-testid='employee'>
+            {employee.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
-
 export default EmployeesList;
